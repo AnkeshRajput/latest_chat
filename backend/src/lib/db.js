11835441e83404b34d88f7bf47c79099737456ee
AnkeshRajput <singhankesh446@gmail.com>
2026-07-10 21:32:46 +0530

    created chatpage

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -1,8 +1,16 @@
 import "dotenv/config";
 import mongoose from "mongoose";
+import dns from "dns";
 
 export async function connectDB() {
   try {
+    // Use reliable public DNS servers for SRV record resolution (fixes ECONNREFUSED on some networks)
+    try {
+      dns.setServers(["1.1.1.1", "1.0.0.1"]);
+    } catch (dnsErr) {
+      // ignore if setting DNS servers is not permitted in the environment
+    }
+
     const uri = process.env.MONGO_URI;
     if (!uri) {
       throw new Error("MONGO_URI is not defined in the environment variables.");
